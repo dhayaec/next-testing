db9b47c2a75fa1799e72b33890b45b74b505c183
dhayaec <dhayaec@gmail.com>
2024-01-07 19:43:48 +0530

    add meta data

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,16 +1,17 @@
+import Head from 'next/head';
+
 type Params = {
   params: {
     slug: string;
   };
 };
 
-export async function generateMetadata({ params }: Params) {
-  return { title: `Post: ${params.slug}` };
-}
-
 export default function Page({ params }: Params) {
   return (
     <>
+      <Head>
+        <title>{params.slug}</title>
+      </Head>
       <h1>Slug: {params.slug}</h1>
     </>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-export const metadata = {
+import Head from 'next/head';
+import Link from 'next/link';
+
+const metadata = {
   title: 'App Router',
 };
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  return (
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+      </Head>
+      <h1>App Router</h1>
+      <Link href={'/blog/hello'}>Blog</Link>
+    </>
+  );
 }
